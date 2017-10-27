2d211eb793d37169f8006eaf12af7f6af085058c
Cosimo Lupo <cosimo@anthrotype.com>
2017-10-27 11:54:21 +0100

    [appveyor] redirect stderr for twine inside powershell
    otherwise PowerShell thinks it's an error... :(
    
    https://ci.appveyor.com/project/anthrotype/zopfli/build/1.0.21/job/4uefbbr3xn8s6r5a#L105
    https://stackoverflow.com/questions/44632416/upload-using-twine-in-appveyor-using-ps

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open("README.rst", "r", encoding="utf-8") as readme:
 
 setup(
     name='zopfli',
-    version='0.1.2',
+    version='0.1.3',
     author='Adam DePrince',
     author_email='deprince@googlealumni.com',
     maintainer='Cosimo Lupo',
